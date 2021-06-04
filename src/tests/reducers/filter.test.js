fb3b780ef50d29a5df5d4035d5690e56608d231c
dorukucak <84201421+dorukucak@users.noreply.github.com>
2021-06-04 13:43:37 +0300

    setup formatting for amount and createdAt

--- src/tests/reducers/filter.test.js
+++ src/tests/reducers/filter.test.js
@@ -1,6 +1,6 @@
 import filtersReducer from '../../reducers/filters';
 import moment from 'moment';
-import { start } from 'live-server';
+
 
 test('should setup default filter values', () => {
  const state = filtersReducer(undefined, { type: '@@INIT'})
