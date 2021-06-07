7109ff37ec4e132aaf3a5bd6a3754cf33a19408a
dorukucak <84201421+dorukucak@users.noreply.github.com>
2021-06-08 01:39:59 +0300

    ok

--- src/tests/components/expensesSummary.test.js
+++ src/tests/components/expensesSummary.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ExpensesSummary } from '../../components/expensesSummary';
+import { ExpensesSummary } from '../../components/ExpensesSummary';
 
 test('should render ExpenseSummary with 1 expense', () => {
     const wrapper = shallow(<ExpensesSummary expenseCount={1} expenseTotal={235}/>);
